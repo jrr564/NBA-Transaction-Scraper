1c417769f6ca9c04a3e99e20c33842486f6b6532
admin <jon.ridewood@gmail.com>
2018-07-02 13:03:29 -0500

    UPDATED THE SCRAPE WOOHOO IT WORKS AGAIN

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -11,19 +11,19 @@ var scrape = function (callback) {
     var $ = cheerio.load(html);
 
     //scrapes data
-    $('tr.row1').each(function (i, element) {
+    $("tr.TableBase-bodyTr ").each(function (i, element) {
 
       var result = {};
       //object
       var a = $(this);
-
-      result.dateoftrans = a.find('td').eq(0).text();
-      result.team = a.find('td').eq(1).text();
-      result.player = a.find('td').eq(2).text();
-      result.transaction = a.find('td').eq(3).text();
+    
+      result.dateoftrans = a.parent().parent().parent().parent().parent().find("h4").text();
+      result.team = a.find("span.CellLogoNameLockup-name").text();
+      result.player = a.find("span.CellPlayerName--long").text();
+      result.transaction = a.children().next().next().text();
       //push data
       articlesArr.push(result);
-      
+      console.log(result);
     });
     callback(articlesArr);
   });
@@ -32,3 +32,18 @@ var scrape = function (callback) {
 
 //exports scrape
 module.exports = scrape;
+
+// $("div.Page-colMain").each(function (i, element) {
+
+//   var result = {};
+//   //object
+//   var a = $(this);
+
+//   result.dateoftrans = a.find("h4").eq(0).text();
+//   result.team = a.children("div").find("td").find("span.CellLogoNameLockup-name").text();
+//   result.player = a.children("div").find("td").find("span.CellPlayerName--long").text();
+//   result.transaction = a.children("div").find("td").next().next().text();
+//   //push data
+//   articlesArr.push(result);
+  
+// });
